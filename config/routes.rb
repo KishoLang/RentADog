31ccac48d4daa7cfd23fdc18151d90fd599597b8
cd ~/code/$RobertPE96/dotfiles && zsh git_setup.sh <robert.p.eickmeyer@gmail.com>
2022-05-07 15:26:25 +0200

    Bookings updated 3.0

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :dogs do
     resources :bookings, only: [:index, :show, :new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
